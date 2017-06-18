bd65b08bcd2bda2c96866b547f6764d69d98b6ce
Janidu Wanigasuriya <janidu.wanigasuriya@rea-group.com>
2017-06-18 13:53:12 +1000

    Fetch experiences

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var link = 'https://janiduw.checkfront.com/api/3.0/item';
+var currentDate = new Date().toISOString();
+
+var classGetUrl = 'https://janiduw.checkfront.com/api/3.0/item?category_id=5&start_date=' + encodeURI(currentDate);
+var experienceGetUrl = 'https://janiduw.checkfront.com/api/3.0/item?category_id=4&start_date=' + encodeURI(currentDate);
+
 var data = {
     baseUrl: "https://janiduw.checkfront.com",
-    classList: null
+    classList: null,
+    experienceList: null
 };
 
-Vue.http.get(link).then(function (response) {
+Vue.http.get(classGetUrl).then(function (response) {
     data.classList = response.data.items;
 }, function (error) {
     console.log(error.statusText);
 });
 
-var example1 = new Vue({
+Vue.http.get(experienceGetUrl).then(function (response) {
+    data.experienceList = response.data.items;
+}, function (error) {
+    console.log(error.statusText);
+});
+
+var classList = new Vue({
     el: '#class-list',
     data: data,
     methods: {
